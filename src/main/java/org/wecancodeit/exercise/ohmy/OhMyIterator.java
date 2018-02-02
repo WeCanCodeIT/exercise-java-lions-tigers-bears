55bf1efb72ad26449457b5a29e1cfbd7d1f8dcfc
B T Forsythe <brian@btforsythe.io>
2018-02-02 17:19:45 -0500

    implementing

--- src/main/java/org/wecancodeit/exercise/ohmy/OhMyIterator.java
+++ src/main/java/org/wecancodeit/exercise/ohmy/OhMyIterator.java
@@ -1,4 +1,24 @@
 package org.wecancodeit.exercise.ohmy;
 
-public class OhMyIterator {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class OhMyIterator implements Iterator<String> {
+
+	private String[] values = { "lions", "tigers", "bears" };
+	
+	private int currentIndex = 0;
+	
+	@Override
+	public boolean hasNext() {
+		return currentIndex < values.length;
+	}
+
+	@Override
+	public String next() {
+		if(!hasNext()) {
+			throw new NoSuchElementException();
+		}
+		return values[currentIndex++];
+	}
 }
